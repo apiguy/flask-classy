101087c57e97addcf45ca191f17c620ccdc345d0
Freedom Dumlao <freedomdumlao@gmail.com>
2013-06-06 10:27:26 -0400

    Cleans up theme and adds google tracking code.

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Flask Classy'
-copyright = u'2012, Freedom Dumlao'
+copyright = u'2013, Freedom Dumlao'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
